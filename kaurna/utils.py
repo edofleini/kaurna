7c869aa9c8152b7ad4a8a80f1b143f64c35438e3
stevenorum <stevenorum@gmail.com>
2015-05-07 16:18:12 -0400

    Adding empty files for the different parts.

---
+++ kaurna/utils.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+# This file will contain the general kaurna functionality needed by both kaurna.writer and kaurna.reader.
---
+++ kaurna/writer.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+# This file will contain tools for writing to kaurna, including both adding and removing credentials.
