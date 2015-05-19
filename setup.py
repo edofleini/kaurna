b784a63653c38758e327ab841e0cf0c67b33b34e
stevenorum <stevenorum@gmail.com>
2015-05-19 17:13:12 -0400

    Adding a basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kaurna',
+      version='1.0',
+      description='Secure secret storage utilities.',
+      author='Quinn Raskin',
+      author_email='quinn@edofleini.com',
+      url='www.edofleini.com',
+      packages=['kaurna'],
+     )
