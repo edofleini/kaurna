ff27588ee7e782e61acf0cc882bb688dd2627fb9
stevenorum <stevenorum@gmail.com>
2015-05-19 17:37:54 -0400

    Restructuring.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='kaurna',
-      version='1.0',
+      version='0.1.0',
       description='Secure secret storage utilities.',
       author='Quinn Raskin',
       author_email='quinn@edofleini.com',
       url='www.edofleini.com',
       packages=['kaurna'],
+      package_dir={'kaurna': 'src/kaurna'},
+      scripts=['scripts/kaurna'],
      )
