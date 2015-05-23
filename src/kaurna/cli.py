8e4aa09175a804cbef509f6aa13d48768419036d
stevenorum <stevenorum@gmail.com>
2015-05-22 23:15:58 -0400

    Finishing off the first cut of the CLI.  I'd forgotten --get-secret.

--- src/kaurna/cli.py
+++ src/kaurna/cli.py
@@ -7,7 +7,7 @@ class CLIDispatcher:
 
     operation_info={
         'list_secrets':{
-            'help':'List the stored secrets.  If you provide --secret-name, only versions of that secret will be shown.  If you provide --secret-name and --secret-version, only the one secret/version will be shown. NOT YET IMPLEMENTED.',
+            'help':'List the stored secrets.  If you provide --secret-name, only versions of that secret will be shown.  If you provide --secret-name and --secret-version, only the one secret/version will be shown.',
             'initial':'l'
             },
         'rotate_keys':{
@@ -19,7 +19,7 @@ class CLIDispatcher:
             'initial':'s'
             },
         'erase_secret':{
-            'help':'Erase the provided secret from kaurna.  Can be used to delete all versions of a secret, but cannot be used to delete all secrets at once.  For that, use --delete-all-the-things. NOT YET IMPLEMENTED.',
+            'help':'Erase the provided secret from kaurna.  Can be used to delete all versions of a secret, but cannot be used to delete all secrets at once.  For that, use --delete-all-the-things.',
             'initial':'e'
             },
         'deprecate_secrets':{
@@ -35,11 +35,11 @@ class CLIDispatcher:
             'initial':'u'
             },
         'get_secret':{
-            'help':'Download the desired secret.  This will print it to stdout; if you don\'t want it to appear on the screen, you can pipe the output of this command to a file or to a clipboard program like pbcopy or xclip (which one to use varies based on your OS). NOT YET IMPLEMENTED.',
+            'help':'Download the desired secret.  This will print it to stdout; if you don\'t want it to appear on the screen, you can pipe the output of this command to a file or to a clipboard program like pbcopy or xclip (which one to use varies based on your OS).',
             'initial':'g'
             },
         'erase_all_the_things':{
-            'help':'Erase every secret.  Only use this as a last resort.  Even if you pass in --force, this will require a prompt. NOT YET IMPLEMENTED.',
+            'help':'Erase every secret.  Only use this as a last resort.  Even if you pass in --force, this will require a prompt.',
             'initial':None
             }
         }
@@ -88,8 +88,7 @@ class CLIDispatcher:
         kaurna.update_secrets(**kwargs)
     
     def get_secret(self, **kwargs):
-        print 'get_secret called, but not yet implemented'
-        exit(1)
+        print kaurna.get_secret(**kwargs)
     
     def erase_all_the_things(self, **kwargs):
         seriously=False
@@ -110,23 +109,23 @@ class CLIDispatcher:
 
     def get_argument_parser(self):
         parser = argparse.ArgumentParser(description='Interact with kaurna from the command line.')
-        operations = parser.add_mutually_exclusive_group()
+        operations = parser.add_mutually_exclusive_group(required=True)
 
         for operation in self.operation_info.keys():
             op = self.operation_info[operation]
             operation_cli = '--{0}'.format(operation.replace('_','-'))
             if op['initial']:
-                operations.add_argument('-{0}'.format(op['initial']), operation_cli, action='store_true', help=op['help'])
+                operations.add_argument('-{0}'.format(op['initial']), operation_cli, action='store_true', help='Operation: {0}'.format(op['help']))
             else:
-                operations.add_argument(operation_cli, action='store_true', help=op['help'])
+                operations.add_argument(operation_cli, action='store_true', help='Operation: {0}'.format(op['help']))
 
-        parser.add_argument('--region', default='us-east-1', help='The AWS region to use.')
-        parser.add_argument('--secret-name', default=None, help='The name of the secret.  Required for erase-secret, store-secret, and get-secret.  Optional for list-secrets, rotate-keys, deprecate-secrets, activate-secrets, and update-secrets.')
-        parser.add_argument('--secret-version', default=None, help='The version of the secret to use.  If this is provided, secret-name must also be provided.  Optional for list-secrets, rotate-keys, store-secret, erase-secrets, deprecate-secrets, activate-secrets, update-secrets, and get-secret.')
-        parser.add_argument('--secret', default=None, help='The secret to store.  Currently the only way to enter it is here, but I\'ll add a way to enter it that doesn\'t display it later.  Required for store-secret.')
-        parser.add_argument('--authorized-entities', nargs='+', help='The entities that should have permission to access the secret(s).  Optional for update-secrets and store-secret; if not provided the empty list will be used.')
-        parser.add_argument('-f', '--force', action='store_true', help='Skip normal confirmation prompts.  Optional for all calls.  Ignored by erase-all-the-things.')
-        parser.add_argument('-v', '--verbose', action='store_true', help='Print random usually-useless information.  May or may not print anything depending on whether or not I\ve implemented it yet, as I haven\'t right now.  Optional for all calls.')
+        parser.add_argument('--region', default='us-east-1', help='Argument: The AWS region to use.')
+        parser.add_argument('--secret-name', default=None, help='Argument: The name of the secret.  Required for erase-secret, store-secret, and get-secret.  Optional for list-secrets, rotate-keys, deprecate-secrets, activate-secrets, and update-secrets.')
+        parser.add_argument('--secret-version', default=None, help='Argument: The version of the secret to use.  If this is provided, secret-name must also be provided.  Optional for list-secrets, rotate-keys, store-secret, erase-secrets, deprecate-secrets, activate-secrets, update-secrets, and get-secret.')
+        parser.add_argument('--secret', default=None, help='Argument: The secret to store.  Currently the only way to enter it is here, but I\'ll add a way to enter it that doesn\'t display it later.  Required for store-secret.')
+        parser.add_argument('--authorized-entities', nargs='+', help='Argument: The entities that should have permission to access the secret(s).  Optional for update-secrets and store-secret; if not provided the empty list will be used.')
+        parser.add_argument('-f', '--force', action='store_true', help='Argument: Skip normal confirmation prompts.  Optional for all calls.  Ignored by erase-all-the-things.')
+        parser.add_argument('-v', '--verbose', action='store_true', help='Argument: Print random usually-useless information.  May or may not print anything depending on whether or not I\ve implemented it yet, as I haven\'t right now.  Optional for all calls.')
 
         return parser
 
@@ -141,7 +140,7 @@ class CLIDispatcher:
         try:
             getattr(self, operation)(**argdict)
         except Exception as e:
-            print e.message
+            print(e.message)
             exit(1)
 
     def do_stuff(self):
